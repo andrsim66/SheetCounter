da579d932fdcc9f36559234962251bb4a84a9465
Andrii <sevenander@gmail.com>
2015-10-03 15:31:23 +0300

    initial

---
+++ app/src/androidTest/java/com/ukgeek/sheetcounter/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ukgeek.sheetcounter.app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
