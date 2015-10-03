0c20e6f1d67e42c537c1dcddd98e735c0252ed0d
Alexandr Voronsky <voronsky@ukr.net>
2015-10-03 21:59:21 +0300

    db

--- app/src/main/java/com/ukgeek/sheetcounter/app/database/DBHelper.java
+++ app/src/main/java/com/ukgeek/sheetcounter/app/database/DBHelper.java
@@ -1,8 +1,13 @@
 package com.ukgeek.sheetcounter.app.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by voronsky on 03.10.15.
@@ -10,6 +15,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String TABLE_NAME = "badWordsDB";
+    public static final String COLUMN_ID = "_id";
     public static final String BAD_WORD = "badWord";
     public static final String SPEECH = "speech";
 
@@ -19,11 +25,52 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        db.execSQL();
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " ("
+                + COLUMN_ID + "INTEGER PRIMARY KEY  AUTOINCREMENT"
+                + BAD_WORD + " TEXT, "
+                + SPEECH + " TEXT, " + " )"
+                + "UNIQUE (_id) ON CONFLICT REPLACE);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public void insertSpeech(SQLiteDatabase db, Speech speech) {
+        ContentValues cv = new ContentValues();
+        cv.put(BAD_WORD, speech.getBadWord());
+        cv.put(SPEECH, speech.getSpeech());
+        db.insertWithOnConflict(TABLE_NAME, null, cv, SQLiteDatabase.CONFLICT_REPLACE);
+    }
+
+    public HashMap<Speech, Speech> selectAllRecords(SQLiteDatabase db) {
+        HashMap<Speech, Speech> records = new HashMap<>();
+
+        Cursor c = db.query(TABLE_NAME, null, null, null, null, null, null);
+        try {
+            if (c != null) {
+                if (c.moveToFirst()) {
+                    do {
+                        Speech badWord = new Speech();
+                        Speech speech = new Speech();
+
+                        badWord.setBadWord(c.getString(c.getColumnIndexOrThrow(BAD_WORD)));
+                        speech.setSpeech(c.getString(c.getColumnIndexOrThrow(SPEECH)));
+
+                        records.put(badWord, speech);
+                    } while (c.moveToNext());
+                }
+            }
+            c.close();
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+
+        return records;
     }
 }
